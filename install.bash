99411a3039171bcf0c44859b69e83eeeb819f9b2
vladastos <vladislavparfeniuc@gmail.com>
2024-03-22 11:27:28 +0100

    added readme, updated install script

--- install.bash
+++ install.bash
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+echo "Installing startEnv..."
 #check if startEnv is already installed
-if (command -v startEnv) then
+if [ -d ~/.config/startEnv ] ; then
     echo "startEnv already installed"
     exit
 fi
 #if not clone the repo 
-if [ ! -d startEnv ] ; then
+if [ ! -d ~/.config/startEnv ] ; then
     echo "cloning startEnv repo..."
     git clone https://github.com/Vladastos/startEnv/scripts/startEnv.py ~/.config/startEnv
     chmod +x ~/.config/startEnv/startEnv.py
+else :
+    echo "startEnv already installed"
 fi
 
 #check if config file is present, if not clone from repo
 if [ ! -f .config/startEnv/config.json ]; then
     echo "Config file not found, cloning from repo..."
     git clone https://github.com/Vladastos/startEnv/config/config.json ~/.config/startEnv 
+else :
+    echo "Config file found"
 fi
 
 
 #check if alias is present in .bash_aliases, if not create
-if ! grep -q "startEnv" ~/.bash_aliases; then
-    echo 'startEnv() {
-    python3 ~/.config/startEnv/startEnv.py "$1"
-}' >> ~/.bash_aliases
+if ! grep -q "startEnv()" ~/.bash_aliases; then
+    echo "alias startEnv='~/.config/startEnv/startEnv.py'" >> ~/.bash_aliases
 fi
 
 
